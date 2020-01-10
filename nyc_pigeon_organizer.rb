5213e65024b6c1ef98fc49125aa86741cbbe0f99
Amy Franz <amy.franz@gmail.com>
2020-01-10 20:49:17 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,17 +6,14 @@ def nyc_pigeon_organizer(data)
   data[:gender][:male].map do |name|
     new_hash[name] = {:color => [], :gender =>[], :lives => []}
   end
-  puts new_hash
   new_hash.each_key do |name|
     data.each_key do |key1|
-  	data[key1].each_key do |key2|
-  	  array_color = []
-  	  puts data[key1][key2].include?(name)
-  	  if data[key1][key2].include?(name) == true
-  	    new_hash[name][key1].unshift(key2[0, key2.size])
-  	    puts "cool"
+  	  data[key1].each_key do |key2|
+  	    array_color = []
+  	    if data[key1][key2].include?(name) == true
+  	      new_hash[name][key1].unshift(key2[0, key2.size])
+  	    end
   	  end
-  	end
   	end
   end
   puts new_hash
