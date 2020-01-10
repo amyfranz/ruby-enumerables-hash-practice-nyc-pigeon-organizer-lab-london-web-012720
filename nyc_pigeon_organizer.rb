694983c1010532c87d054dc63684aaa291b0c6d1
Amy Franz <amy.franz@gmail.com>
2020-01-10 20:38:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data[:gender][:female].map do |name|
+    new_hash[name] = {:color => [], :gender =>["female"], :lives => []}
+  end
+  data[:gender][:male].map do |name|
+    new_hash[name] = {:color => [], :gender =>["male"], :lives => []}
+  end
+  puts new_hash
+  new_hash.each_key do |name|
+    puts 1
+  	data[:color].each_key do |test|
+  	  array_color = []
+  	  puts data[:color][test].include?(name)
+  	  if data[:color][test].include?(name) == true
+  	    new_hash[name][:color].unshift(test)
+  	    puts "cool"
+  	  end
+  	end
+  end
+  puts new_hash
+  return new_hash
 end
